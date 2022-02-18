10aee21b2344f1f528b5fce85fb010372b8772a9
niculas1989 <nicolas.maranzano@libero.it>
2022-02-18 11:34:05 +0100

    added first pieces in #header-top

--- src/components/HeadJumbotron.vue
+++ src/components/HeadJumbotron.vue
@@ -1,10 +1,33 @@
 <template>
   <div>
     <div id="header-top">
-      <div class="container">
-        <nav class="d-flex align-items-center justify-content-between">
-          <section id="left-info">PROVA</section>
-          <section id="right-info">PROVA</section>
+      <div class="container h-100">
+        <nav
+          v-for="(i, index) in info"
+          :key="index"
+          class="d-flex align-items-center justify-content-between h-100"
+        >
+          <section id="left-info">
+            <span>
+              <i class="fa-solid fa-clock"></i>
+              Open Hours: {{ i.open }}</span
+            >
+          </section>
+          <section id="right-info">
+            <span>{{ i.tel }}</span>
+            <span>{{ i.email }}</span>
+            <span>
+              <a href="#">
+                <i class="fa-brands fa-facebook-f"></i>
+              </a>
+              <a href="#">
+                <i class="fa-brands fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i class="fa-brands fa-linkedin-in"></i>
+              </a>
+            </span>
+          </section>
         </nav>
       </div>
     </div>
@@ -22,5 +45,9 @@ export default {
 #header-top {
   height: 50px;
   background-color: #778a94;
+  color: #e3e7df;
+  .fa-brands {
+    color: #e3e7df;
+  }
 }
 </style>
