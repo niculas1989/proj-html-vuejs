ebdc2372a7abfded8ad30282b92e09bf60a88280
niculas1989 <nicolas.maranzano@libero.it>
2022-02-18 14:39:07 +0100

    added new section - working on other components

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>qualcosa andrà qua</div>
+  <div>
+    <section id="performance-section">
+      <div
+        class="container h-100 d-flex align-items-center justify-content-around"
+      >
+        <div
+          class="square me-2 d-flex align-items-center justify-content-center"
+        >
+          contenuto
+        </div>
+        <div
+          class="square me-2 d-flex align-items-center justify-content-center"
+        >
+          contenuto
+        </div>
+        <div class="square d-flex align-items-center justify-content-center">
+          contenuto
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -7,4 +27,16 @@ export default {};
 </script>
 
 <style scoped lang="scss">
+@import "../assets/scss/_vars.scss";
+
+#performance-section {
+  height: 240px;
+  background-color: $background_first_section;
+}
+
+.square {
+  height: 130px;
+  width: 180px;
+  background-color: #fff;
+}
 </style>
