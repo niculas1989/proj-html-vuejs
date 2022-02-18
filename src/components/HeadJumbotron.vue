87d514d16db06dfde238d7db1ccbec0d0f6bcd40
niculas1989 <nicolas.maranzano@libero.it>
2022-02-18 11:20:57 +0100

    added first steps for HeadJumbotron.vue

--- src/components/HeadJumbotron.vue
+++ src/components/HeadJumbotron.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <div v-for="(i, index) in info" :key="index">
-      <p>{{ i.open }}</p>
+    <div id="header-top">
+      <div class="container">
+        <nav class="d-flex align-items-center justify-content-between">
+          <section id="left-info">PROVA</section>
+          <section id="right-info">PROVA</section>
+        </nav>
+      </div>
     </div>
+    <div id="jumbotron"></div>
   </div>
 </template>
 
@@ -13,4 +19,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
+#header-top {
+  height: 50px;
+  background-color: #778a94;
+}
 </style>
