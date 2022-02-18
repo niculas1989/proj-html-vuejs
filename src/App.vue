5750c4363af177f63f3627fa348b057cc4bf2bd7
niculas1989 <nicolas.maranzano@libero.it>
2022-02-18 11:03:46 +0100

    added first elements for Header.vue (in App)

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,26 @@
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+  data() {
+    return {
+      headerInfo: {
+        open: "Mon-Sat-9:00-18:00",
+        tel: "+1(305) 1234-5678",
+        email: "hello@example.com",
+      },
+    };
+  },
 };
 </script>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>qualcosa andrà qua</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+</style>
